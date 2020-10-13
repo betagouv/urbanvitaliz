b19668f87b9e7ba686434323857ac009053f7a36
David Bruant <davidbruant@protonmail.com>
2020-10-13 17:09:16 +0200

    configuration du nom de la base de données pour Clever Cloud

--- database/constants.cjs
+++ database/constants.cjs
@@ -5,6 +5,6 @@ module.exports = {
         FRICHES_COLLECTIONS: 'friches_collection',
         FRICHES: 'friches',
     }),
-    DATABASE_NAME: "urban_vitaliz",
+    DATABASE_NAME: process.env.MONGODB_ADDON_DB || "urban_vitaliz",
     MONGO_URL: process.env.MONGODB_ADDON_URI || `mongodb://127.0.0.1:27017`
 }
