d129db70abaa63ae53ea1ef2d07940d046939b8a
David Bruant <davidbruant@protonmail.com>
2021-02-10 14:41:41 +0100

    Suppression des étiquettes d'étape et de thématique

--- scripts/assistant/components/ResourceOverview.svelte
+++ scripts/assistant/components/ResourceOverview.svelte
@@ -8,12 +8,7 @@
 
 <article>
     <a href={resource.url} target="_blank"><h1>{resource.attributes.phrase_catch}</h1></a>
-    <footer>
-        <span class="étape">{resource.attributes.etape}</span>
-        {#each thematiques as thematique}
-            <span class="thematique">{thematique}</span>
-        {/each}
-    </footer>
+
 </article>
 
 <style>
@@ -34,21 +29,6 @@
         width: 30%;
         height: 3px;
         background-color: #073763;
-    }
-
-    footer .étape, footer .thematique{
-        font-size: 0.6rem;
-        padding: 0.2rem;
-        border-radius: 0.5rem;
-
-        white-space: nowrap;
-    }
-
-    footer .étape{
-        background-color: lightseagreen;
-    }
-    footer .thematique{
-        background-color: lightpink;
     }
 
 </style>
