6164da6a6929075a2834896b96b162d2f7bbd397
David Bruant <davidbruant@protonmail.com>
2021-03-12 15:13:11 +0100

    Correction de l'origine du serveur clever cloud

--- scripts/spa-main.js
+++ scripts/spa-main.js
@@ -13,7 +13,7 @@ import getAllResources from './getAllResources.js';
 
 const isProduction = location.hostname === 'betagouv.github.io'
 const SERVER_ORIGIN = isProduction ? 
-    'https://app-f92a129e-7c5e-4922-97ab-66be747554dd.cleverapps.io' :
+    'https://app-20420772-6ed7-40ca-978c-f360edf8941c.cleverapps.io' :
     `http://localhost:4999`
 
 console.log('API server origin:', SERVER_ORIGIN)
