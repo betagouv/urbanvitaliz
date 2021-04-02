6c26ed5e10e7c929b674b6bf632eb54b2d843f6e
elodie pelcat <elodie.pelcat@gmail.com>
2021-04-02 13:11:41 +0200

    retire un import

--- scripts/header-login.js
+++ scripts/header-login.js
@@ -1,6 +1,5 @@
 import prepareLoginHearder from './prepareLoginHeader.js'
 import makeListRessourceURLFromPerson from './makeListRessourceURLFromPerson.js'
-import baseUrl from './baseUrl.js';
 
 console.log("BONJOIR 🦄 ")
 
