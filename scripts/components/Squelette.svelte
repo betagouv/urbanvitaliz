dd591492c3ca5b8b6109cfb103c867bde01467e2
elodie pelcat <elodie.pelcat@gmail.com>
2021-04-04 13:44:56 +0200

    mise en place des fragment avec svelte

---
+++ scripts/components/Squelette.svelte
@@ -0,0 +1,78 @@
+<div class="flex-container">
+    <section class="colonne-de-gauche">
+        <nav class="rf-sidemenu" aria-label="Menu latéral">
+            <div class="rf-sidemenu__inner">
+                <button
+                    class="rf-sidemenu__btn"
+                    hidden
+                    aria-controls="rf-sidemenu-wrapper"
+                    aria-expanded="false">Dans cette rubrique</button>
+                <div class="rf-collapse" id="rf-sidemenu-wrapper">
+                    <ul class="rf-sidemenu__list">
+                        <!-- <li class="rf-sidemenu__item rf-sidemenu__item--active">
+                    <a class="rf-sidemenu__link" href="#" target="_self" aria-current="page">Accès direct</a>
+                </li> -->
+                        <li class="rf-sidemenu__item">
+                            <a class="rf-sidemenu__link" href="./toute-les-ressoures" target="_self"><span class="rf-fi-file-fill" aria-hidden="true"></span>Rechercher une ressource</a>
+                        </li>
+                        <li class="rf-sidemenu__item">
+                            <a class="rf-sidemenu__link" href="./toute-les-ressoures" target="_self"><span class="rf-fi-file-fill" aria-hidden="true"></span>Mes ressources</a>
+                        </li>
+                        <li class="rf-sidemenu__item">
+                            <a class="rf-sidemenu__link" href="./toute-les-ressoures" target="_self"><span class="rf-fi-file-fill" aria-hidden="true"></span>Toutes les ressources</a>
+                        </li>
+                        <li class="rf-sidemenu__item">
+                            <a class="rf-sidemenu__link" href="./toute-les-ressoures" target="_self"><span class="rf-fi-file-fill" aria-hidden="true"></span>Méthodologie</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </section>
+    <section class="colonne-du-centre">
+        <slot name="colonne-du-centre"></slot>
+    </section>
+    
+	<section class="colonne-de-droite">
+	    <slot name="colonne-de-droite"></slot>
+    </section>
+</div>
+
+
+
+<style lang="scss">
+    @import "../../node_modules/@gouvfr/dsfr/packages/schemes/src/styles/settings/_colors.scss";
+
+    .flex-container{
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+    }
+    .colonne-de-gauche{
+        background-color: #F8F8F8;
+        flex: 3;
+    }
+    .rf-sidemenu{
+        padding-right: 0
+    }
+    .rf-sidemenu__inner{
+        width: 17rem;
+        border: none;
+    }
+    .rf-sidemenu__link{
+        font-size: 16px;
+        line-height: 1.5em;
+    }
+    .rf-fi-file-fill{
+        padding-right: 0.5rem;
+    }
+    .colonne-du-centre{
+        padding: 0.5rem 1rem;
+        flex: 10;
+    }
+    .colonne-de-droite{
+        background-color: hsl(240, 36.8%, 96.3%);
+        flex: 3;
+
+    }
+</style>
--- scripts/components/ToutesLesRessources.svelte
+++ scripts/components/ToutesLesRessources.svelte
@@ -1,6 +1,7 @@
 <script>
     import ResourceFilters from "./ResourceFilters.svelte";
     import ResourceList from "./ResourceList.svelte";
+    import Squelette from "./Squelette.svelte";
 
     export let étapes;
     export let thématiques;
@@ -14,44 +15,11 @@
 
     export let makeBookmarkResource;
     export let makeUnbookmarkResource;
-    export let urlSecret;
-    
+    //export let urlSecret;
 </script>
 
-<div class="flex-container">
-    <section class="colonne-de-gauche">
-        <nav class="rf-sidemenu" aria-label="Menu latéral">
-            <div class="rf-sidemenu__inner">
-                <button
-                    class="rf-sidemenu__btn"
-                    hidden
-                    aria-controls="rf-sidemenu-wrapper"
-                    aria-expanded="false">Dans cette rubrique</button
-                >
-                <div class="rf-collapse" id="rf-sidemenu-wrapper">
-                    <ul class="rf-sidemenu__list">
-                        <!-- <li class="rf-sidemenu__item rf-sidemenu__item--active">
-                    <a class="rf-sidemenu__link" href="#" target="_self" aria-current="page">Accès direct</a>
-                </li> -->
-                        <li class="rf-sidemenu__item">
-                            <a class="rf-sidemenu__link" href="#" target="_self"><span class="rf-fi-file-fill" aria-hidden="true"></span>Rechercher une ressource</a>
-                        </li>
-                        <li class="rf-sidemenu__item">
-                            <a class="rf-sidemenu__link" href="#" target="_self"><span class="rf-fi-file-fill" aria-hidden="true"></span>Mes ressources</a>
-                        </li>
-                        <li class="rf-sidemenu__item">
-                            <a class="rf-sidemenu__link" href="#" target="_self"><span class="rf-fi-file-fill" aria-hidden="true"></span>Toutes les ressources</a>
-                        </li>
-                        <li class="rf-sidemenu__item">
-                            <a class="rf-sidemenu__link" href="#" target="_self"><span class="rf-fi-file-fill" aria-hidden="true"></span>Méthodologie</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </section>
-
-    <section class="colonne-du-centre">
+<Squelette>
+    <svelte:fragment slot="colonne-du-centre">
         <h3>Toutes les Ressources</h3>
 
         <ResourceFilters
@@ -67,48 +35,13 @@
             {makeUnbookmarkResource}
             {bookmarkedResourceIdSet}
         />
-    </section>
-
-    <section class="colonne-de-droite">
-        
-    </section>
-</div>
+    </svelte:fragment>
+</Squelette>
 
 <style lang="scss">
     @import "../../node_modules/@gouvfr/dsfr/packages/schemes/src/styles/settings/_colors.scss";
 
-    .flex-container{
-        display: flex;
-        flex-direction: row;
-    }
-    .colonne-de-gauche{
-        background-color: #F8F8F8;
-        flex: 3;
-    }
-    .rf-sidemenu{
-        padding-right: 0
-    }
-    .rf-sidemenu__inner{
-        width: 17rem;
-        border: none;
-    }
-    .rf-sidemenu__link{
-        font-size: 16px;
-        line-height: 1.5em;
-    }
-    .rf-fi-file-fill{
-        padding-right: 0.5rem;
-    }
-    h3{
-       color: $blue-france-500; 
-    }
-    .colonne-du-centre{
-        padding: 0.5rem 1rem;
-        flex: 10;
-    }
-    .colonne-de-droite{
-        background-color: hsl(240, 36.8%, 96.3%);
-        flex: 3;
-
+    h3 {
+        color: $blue-france-500;
     }
 </style>
